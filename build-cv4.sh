d96a584f6ae078d15a414b1424a35482e2e777d6
Caelin <caelinsutch@gmail.com>
2020-05-14 13:38:58 -0700

    Added script

---
+++ build-cv4.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential cmake unzip pkg-config
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk-3-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install python3-dev
+cd ~
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.2.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.2.0.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+mv opencv-4.2.0 opencv
+mv opencv_contrib-4.2.0 opencv_contrib
+cd ~/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+  -D SHARED=off \
+  -D INSTALL_C_EXAMPLES=OFF \
+  -D OPENCV_ENABLE_NONFREE=ON \
+  -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ..
+make -j4
+mkdir cv4
+mv opencv cv4/
+mv opencv_contrib cv4/
+
+#sudo make install
+#sudo ldconfig
