5f30c4892ba2893b8b12c3e687dd71c8858e6cf9
Caelin <caelinsutch@gmail.com>
2020-05-14 17:31:20 -0700

    Fastjar time

--- install-cv4.sh
+++ install-cv4.sh
@@ -2,6 +2,7 @@
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
 git lfs install
-sudo apt-get install unzip
+sudo apt-get install fastjar
 git lfs fetch
-sudo unzip ./cv4-nano.zip -d /usr/include
+jar xvf ./cv4-nano.zip
+sudo mv ./cv4/* /usr/include
