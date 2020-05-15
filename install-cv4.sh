c5c296bb2a54a90fa0b945fbc1b5b4ae8a95ed2f
Caelin <caelinsutch@gmail.com>
2020-05-15 15:13:41 -0700

    Moved to using azure blob

--- install-cv4.sh
+++ install-cv4.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
-git lfs install
 sudo apt-get install fastjar
-git lfs fetch
+curl -L -o cv4-nano.zip https://brbinaries.blob.core.windows.net/cv4/cv4.zip
 jar xvf ./cv4-nano.zip
 sudo mv ./cv4/* /usr/include
