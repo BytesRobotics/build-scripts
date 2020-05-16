0c11248db5fb6f0e9b9f2d9da1d644d926454932
Caelin <caelinsutch@gmail.com>
2020-05-16 13:19:18 -0700

    Quet mode on install cv4 unzip

--- install-cv4.sh
+++ install-cv4.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get install fastjar
 curl -L -o cv4-nano.zip https://brbinaries.blob.core.windows.net/cv4/cv4.zip
-jar xvf ./cv4-nano.zip
+jar xvf -q ./cv4-nano.zip
 sudo mv ./cv4/* /usr/include
 sudo ldconfig
 sudo rm -R cv4-nano.zip
