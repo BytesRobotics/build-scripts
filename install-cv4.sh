f8c93cdce5f3461f9aa87e72d7a29147e51d7acc
Caelin <caelinsutch@gmail.com>
2020-05-18 14:42:04 -0700

    Added descirption

--- install-cv4.sh
+++ install-cv4.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 sudo apt-get install unzip
+echo "------------------------------------------------------"
+echo "GETTING ZIPPED BINARIES"
+echo "------------------------------------------------------"
 curl -L -o cv4-nano.zip https://brbinaries.blob.core.windows.net/cv4/cv4.zip
+echo "------------------------------------------------------"
+echo "Unzipping"
+echo "------------------------------------------------------"
 unzip -q ./cv4-nano.zip
 sudo mv ./cv4/* /usr/include
+echo "------------------------------------------------------"
+echo "Configuring"
+echo "------------------------------------------------------"
 sudo ldconfig
 sudo rm -R cv4-nano.zip
