2887e16f35e5c61ebba490875ff4af6d20de4b81
Caelin <caelinsutch@gmail.com>
2020-05-18 13:09:31 -0700

    Updated to unzip

--- install-cv4.sh
+++ install-cv4.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-get install fastjar
+sudo apt-get install unzip
 curl -L -o cv4-nano.zip https://brbinaries.blob.core.windows.net/cv4/cv4.zip
-jar xvf -q ./cv4-nano.zip
+unzip -q ./cv4-nano.zip
 sudo mv ./cv4/* /usr/include
 sudo ldconfig
 sudo rm -R cv4-nano.zip
